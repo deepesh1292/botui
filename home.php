c740c6e19ed9a0eb3278a16a1c680366a140e965
Deepesh Shrivastava <Deepesh.Shrivastava@NIIT-Tech.com>
2020-04-13 17:32:49 +0530

    js

--- home.php
+++ home.php
@@ -1,1 +1,4 @@
 <?php include_once("index.html"); ?>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script  src="js/index.js"></script>
