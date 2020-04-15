55e4f6926514e818eae27b53619e21e426f0ab0d
Deepesh Shrivastava <Deepesh.Shrivastava@NIIT-Tech.com>
2020-04-15 10:18:45 +0530

    removing php

--- home.php
+++
@@ -1,4 +0,0 @@
-<?php include_once("index.html"); ?>
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script  src="js/index.js"></script>
